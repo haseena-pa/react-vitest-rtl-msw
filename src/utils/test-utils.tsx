513a775403f75a698ca6eca2183e736e598f4066
haseena-pa <haseenakhader.uk@gmail.com>
2024-07-05 14:43:08 +0100

    vdhasdhasdghasd

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -11,7 +11,7 @@ function customRender(ui: React.ReactElement, options = {}) {
     ...options,
   });
 }
-
+// eslint-disable-next-line react-refresh/only-export-components
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
 // override render export
